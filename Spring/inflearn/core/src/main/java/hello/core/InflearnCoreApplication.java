b97bf6524cf34cb123bdc694cf113c724773c5b0
Juuunew <hyukjuuun@gmail.com>
2021-12-05 15:46:28 +0900

    inflearn Spring Study

---
+++ Spring/inflearn/core/src/main/java/hello/core/InflearnCoreApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnCoreApplication.class, args);
+	}
+
+}
