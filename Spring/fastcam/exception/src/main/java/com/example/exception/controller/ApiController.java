81c81c8e9782d4a7e27d6693d63095844af71d59
Juuunew <hyukjuuun@gmail.com>
2021-12-31 13:39:59 +0900

    Spring Boot - Validation / Exception

--- Spring/fastcam/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ Spring/fastcam/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -1,25 +1,30 @@
 package com.example.exception.controller;
 
 import com.example.exception.dto.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(@Size(min = 2)
+                    @RequestParam String name,
+
+                    @NotNull
+                    @Min(1)
+                    @RequestParam Integer age) {
         User user = new User();
 
         user.setName(name);
         user.setAge(age);
-
-        int a = 10 + age;
 
         return user;
     }
@@ -33,9 +38,9 @@ public class ApiController {
     /**
      * Controller 에 위치해두면 그 Controller 의 Exception 에만 관여함
      */
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+/*    @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e) {
         System.out.println("api controller");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+    }*/
 }
