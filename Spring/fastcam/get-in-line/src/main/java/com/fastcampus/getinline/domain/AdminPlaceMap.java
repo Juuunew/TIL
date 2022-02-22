1b349be09853b72bc12f3832c5548f3d7d04c483
Juuunew <hyukjuuun@gmail.com>
2022-02-22 17:08:38 +0900

    List 검색 구현

---
+++ Spring/fastcam/get-in-line/src/main/java/com/fastcampus/getinline/domain/AdminPlaceMap.java
@@ -0,0 +1,18 @@
+package com.fastcampus.getinline.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AdminPlaceMap {
+
+    private Long id;
+
+    private Long adminId;
+    private Long PlaceId;
+
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+
+}
