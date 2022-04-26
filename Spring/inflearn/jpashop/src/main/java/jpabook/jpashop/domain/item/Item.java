3cf34c2bc56dc0f3d81beecca280c870482f32cc
hyukjun <hyukjuuun@gmail.com>
2022-04-26 22:29:30 +0900

    Spring JPA - 웹 계층 개발

--- Spring/inflearn/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Spring/inflearn/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -10,21 +10,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
+@Setter
 public abstract class Item {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "item_id")
     private Long id;
 
     private String name;
     private int price;
     private int stockQuantity;
-
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
+    private List<Category> categories = new ArrayList<Category>();
 
     //==비즈니스 로직==//
 
